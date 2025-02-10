2061c9ff24a94d87a8133c056bb053afe115c9da
Olexandrd <olexandrd@gmail.com>
2025-02-10 19:50:15 +0200

    Add validation schemas and implement create/update contact functions

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,4 +1,8 @@
 import contactsService from "../services/contactsServices.js";
+import {
+  createContactSchema,
+  updateContactSchema,
+} from "../schemas/contactsSchemas.js";
 
 export const getAllContacts = async (req, res) => {
   const contacts = await contactsService.listContacts();
@@ -23,6 +27,18 @@ export const deleteContact = async (req, res) => {
   }
 };
 
-export const createContact = (req, res) => {};
+export const createContact = async (req, res) => {
+  const { error } = createContactSchema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.message });
+  }
+  const newContact = await contactsService.addContact(req.body);
+  res.json(newContact);
+};
 
-export const updateContact = (req, res) => {};
+export const updateContact = (req, res) => {
+  const { error } = updateContactSchema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
