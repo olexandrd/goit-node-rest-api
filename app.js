188fda6f13d8846631598b5b6cffe81e48c2d68c
Olexandrd <olexandrd@gmail.com>
2025-03-02 23:02:52 +0200

    Add static file serving

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static("public"));
 app.use("/api/contacts", contactsRouter);
 app.use("/api/auth", authRouter);
 
